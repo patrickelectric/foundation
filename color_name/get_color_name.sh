dd6b3bb2f1ba03e3f22d578da81af2040e4e9d73
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-11 12:30:49 -0300

    color_name: Allow int usage as argument
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- color_name/get_color_name.sh
+++ color_name/get_color_name.sh
@@ -7,7 +7,7 @@ error() {
 }
 
 usage() {
-    echo "USAGE: $PROG 000000"
+    echo "USAGE: $PROG 000000 or $PROG 00,00,00"
 }
 
 if [[ $# -eq 1 ]]; then
@@ -16,4 +16,17 @@ else
     usage && error "Incorrect number of arguments: $#"
 fi
 
+# Check if value is r,g,b (int)
+RGB=$(echo $COLOR | grep -oP "\d{1,3},\d{1,3},\d{1,3}")
+
+# If is r,g,b (int) transform it to hexadecimal value and format #RRGGBB
+if [[ $RGB != "" ]]; then
+    COLOR=""
+    for i in $(echo $RGB | tr "," "\n"); do
+        COLOR="$COLOR$(echo "obase=16; $i" | bc | awk '{printf("%02s", $1)}')"
+    done
+    COLOR=${COLOR// /0}
+fi
+
+echo "Search for color: #$COLOR"
 curl -s --compressed https://api.color.pizza/v1/${COLOR} |  python -m json.tool
