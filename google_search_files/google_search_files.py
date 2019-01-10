9d9b0b5761c6d629b631ed0f0ebedba3e59ed77d
Patrick José Pereira <patrickelectric@gmail.com>
2019-01-10 00:31:52 +0000

    google_search_files: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ google_search_files/google_search_files.py
@@ -0,0 +1,32 @@
+import argparse
+
+# from https://github.com/abenassi/Google-Search-API
+from google.google import search
+
+parser = argparse.ArgumentParser(
+    description="Find files in google.",
+    epilog='python google_search_files --format=pdf datasheet 555'
+)
+parser.add_argument('--formats', action="store", type=str, nargs='+', required=True, default='pdf',
+    help="Format to be searched."
+)
+parser.add_argument('text', nargs='*')
+
+args = parser.parse_args()
+
+def find(text, formats):
+    filetype = ''
+
+    msg = "\U0001F4C4 Hey there, champs! I found these files on Google:\n------------------------------\n"
+
+    filetype = ' filetype:'.join(formats)
+
+    text += filetype
+    results = search(text)
+
+    for result in results[:7]:
+        msg = msg + '{:<70s}{:>100s}\n------------------------------\n'.format(result.name[:70], result.link)
+    return msg
+
+if __name__ == '__main__':
+    print(find(' '.join(args.text), args.formats))
