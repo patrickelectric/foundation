f47b1232885f947488159ccf7ec4f8a826e201fe
Patrick José Pereira <patrickelectric@gmail.com>
2019-03-22 12:16:14 -0300

    github_move_issues: Close the old ones and update other features
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- github_move_issues/githubIssueMover.py
+++ github_move_issues/githubIssueMover.py
@@ -38,29 +38,44 @@ class GithubIssueMover:
             for _ in issues:
                 total = total + 1
             for i, issue in enumerate(issues):
+                upstream_tag = False
+                confirm_tag = False
+
+                tag = ''
+                for label in issue.labels:
+                    if label.name == 'upstream':
+                        upstream_tag = True
+                    elif label.name == 'confirm':
+                        confirm_tag = True
+                    else:
+                        tag = label.name.title()
+                if not (not confirm_tag and upstream_tag):
+                    continue
+
                 # GitHub's REST API v3 considers every pull request an issue, but not every issue is a pull request.
                 # You can identify pull requests by the pull_request key.
                 if issue.pull_request:
                     continue
+
+                print("--------")
                 print('[ISSUE][%s][%d/%d]\t%s %s' % (issue_state, i + 1, total, repo.name, issue.title))
+                title = '%s: %s' % (tag, issue.title)
+                print(title)
                 print('Moved from: %s#%d' % (repo.full_name, issue.number))
                 print('Original author: @%s' % (issue.user.login))
-                print('Body: @%s' % (issue.body))
-                title = 'Sub: %s' % (issue.title)
+                issue_body = issue.body.strip()
+                if not issue_body:
+                    issue_body = '**No description provided.** :sleeping:'
                 body = \
 '''
 Moved from: %s#%d
 Original author: @%s
 
-### Feature request
+### Issue description
 %s
+'''% (repo.full_name, issue.number, issue.user.login, issue_body)
+                print(body)
 
-**Platform**
-[ ] All
-[ ] AntennaTracker
-[ ] Copter
-[ ] Plane
-[ ] Rover
-[x] Submarine
-'''% (repo.full_name, issue.number, issue.user.login, issue.body)
+                # close issue and create a new one
+                issue.edit(state='closed')
                 repo_out.create_issue(title=title, body=body)
